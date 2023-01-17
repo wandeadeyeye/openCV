49752ceef7f36e7a93255acccf2bb9af507bce90
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-01-17 19:51:08 +0100

    update

--- 01. OpenCV With Python - Introduction & Images.py
+++ 01. OpenCV With Python - Introduction & Images.py
@@ -14,11 +14,11 @@ img = cv2.resize(img, (0,0), fx= 0.6, fy= 0.6)
 # rotating an image clockwise
 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-# save an image
-cv2.imwrite('01_new_apple.jpg', img)
-
 # display the image
 cv2.imshow('Image', img)
+
+# save an image
+cv2.imwrite('01_new_apple.jpg', img)
 
 # wait time after image is loaded '0' = infinite, '10' = 10 seconds
 cv2.waitKey(0)
