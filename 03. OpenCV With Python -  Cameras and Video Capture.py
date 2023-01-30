478a3f455ddd478d5759743c5568544ecd26a82f
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-01-30 21:16:17 +0100

    loaded file update

--- 03. OpenCV With Python -  Cameras and Video Capture.py
+++ 03. OpenCV With Python -  Cameras and Video Capture.py
@@ -3,11 +3,12 @@ import numpy as np
 import cv2
 
 #load the video file
-file = cv2.VideoCapture('assets/vid.mp4')
+file = cv2.VideoCapture('assets/zuck.mp4')
 
 #loop
 while True:
 	ret, frame = file.read()		
+	
 	cv2.imshow('frame', frame)
 
 	if cv2.waitKey(1) == ord('e'):
