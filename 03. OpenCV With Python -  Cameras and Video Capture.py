ba446d5fd1fca3f9cc6dcba9d81c7b4e0b1ebaa7
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-01-31 20:40:56 +0100

    update 03

--- 03. OpenCV With Python -  Cameras and Video Capture.py
+++ 03. OpenCV With Python -  Cameras and Video Capture.py
@@ -7,9 +7,18 @@ file = cv2.VideoCapture('assets/zuck.mp4')
 
 # loop
 while True:
-	ret, frame = file.read()		
+	ret, frame = file.read()
+	width = int(cap.get(3))
+	height = int(cap.get(4))
+
+	video = np.zeros(frame.shape, np.uint8)
+	smaller_frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+	video[:height//2, :width//2] = smaller_frame
+	video[:height//2, :width//2] = smaller_frame
+	video[:height//2, :width//2] = smaller_frame
+	video[:height//2, :width//2] = smaller_frame
 	
-	cv2.imshow('frame', frame)
+	cv2.imshow('frame', video)
 
 	if cv2.waitKey(1) == ord('e'):
 		break
